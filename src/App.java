0f57c34785563b59daa741774b81f6bac043eb0d
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-17 13:22:55 -0300

    compareTo

--- src/App.java
+++ src/App.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entities.Employee;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        List<String> list  = new ArrayList<>() ;
+        List<Employee> list  = new ArrayList<>() ;
         String path  = "/home/luiz/wk/udemy/java/interface/interface001/interface-comparable/names/names.txt";
 
         try (BufferedReader br  = new BufferedReader(new FileReader(path))){
-            String name = br.readLine();
-            while(name != null){
-                list.add(name);
-                name = br.readLine();
+            String employeeCsv = br.readLine();
+            while(employeeCsv != null){
+                String [] fields =  employeeCsv.split(",");
+                list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+                employeeCsv = br.readLine();
             }
             Collections.sort(list);
-            for (String s : list){
-                System.out.println(s);
+            for (Employee employee : list){
+                System.out.println(employee.getName() + " , " + employee.getSalary());
             }
         } catch (Exception e) {
             System.out.println("Error" + e.getMessage());
